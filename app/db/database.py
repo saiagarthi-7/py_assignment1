810f5ceeed15a0fca91b04a30d9f100c4f932a12
saiagarthi-7 <saimanikya.agarthi@aifalabs.com>
2025-03-10 19:36:08 +0530

    also implemented JWT

--- app/db/database.py
+++ app/db/database.py
@@ -1,10 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
+load_dotenv()
+DB = os.getenv('SQLALCHEMY_DATABASE_URL', "postgresql://postgres:root@localhost/employee_db")
 
-SQLALCHEMY_DATABASE_URL="postgresql://postgres:root@localhost/employee_db"
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(DB)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
--- app/main.py
+++ app/main.py
@@ -1,19 +1,42 @@
-from fastapi import FastAPI,HTTPException
+from fastapi import FastAPI
 from api.routes import employees
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
+from jose import jwt
 
 app=FastAPI()
+
+app.include_router(employees.router)
+
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=['*'],    #allow which domains can access the fastapi
     allow_credentials=True, #allow the domains based on the 
     allow_methods=['*'],    #allow specific methods like get, put, post, delete
-    allow_headers=['*']   
+    allow_headers=['*']
 )
 
-app.include_router(employees.router)
+SECRET_KEY = 'nmljhgfdsdfghu876543ewdfghj'
+ALGORITHM = 'HS256'
+
+def create_access_token(subject: str):
+    token = jwt.encode({'data' : subject}, SECRET_KEY, algorithm = ALGORITHM)
+    return {'access_token' : token}
+
+@app.get('/create-token')
+def create_token_api(name : str):
+    token = create_access_token(subject = name)
+    return token
+
+def decode_token(token: str):
+    data = jwt.decode(token, SECRET_KEY, algorithms = [ALGORITHM])
+    return data
+
+@app.get('/decode-token')
+def decode_token_api(token : str):
+    data = decode_token(token = token)
+    return data
 
 if "__main__" == __name__:
     uvicorn.run(app,port=8090,reload=True)
